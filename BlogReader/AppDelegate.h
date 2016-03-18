bb23ddd6d394fc535494d1259cefef65c86a7291
Brandon <leebran@onid.oregonstate.edu>
2016-03-18 00:30:12 -0700

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Brandon Lee on 3/18/16.
+//  Copyright © 2016 Brandon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReader
+//
+//  Created by Brandon Lee on 3/18/16.
+//  Copyright © 2016 Brandon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
